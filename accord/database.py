f041d71c9d3fc2cd8a6d37e9610cd10384ac19a2
HSAkash <hemelakash472@gmail.com>
2025-02-04 20:48:51 +0600

    vector store path added

--- accord/database.py
+++ accord/database.py
@@ -2,16 +2,13 @@ import sqlite3
 import os
 from pathlib import Path
 from accord import logger
-from accord.constants import (
-    CONCATENATE_EMBEDDED_FILE_PATH,
-    CONCATENATE_DOCUMENT_FILE_PATH,
-    
-)
+from accord.utils import get_config
 
 class Database:
     def __init__(self):
         self.connection = sqlite3.connect(Path("accord.db"))
         self.crsr = self.connection.cursor()
+        self.config = get_config()
 
 
     def create_table(self):
@@ -27,28 +24,27 @@ class Database:
         # SQL command to create a table in the database
         sql_command = """CREATE TABLE document ( 
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        name TEXT NOT NULL, 
-        file_path TEXT NOT NULL,
+        name TEXT NOT NULL,
         vector_path TEXT NOT NULL);"""
         # execute the statement
         self.crsr.execute(sql_command)
 
-        self.insert_data('concatenate.pdf', CONCATENATE_DOCUMENT_FILE_PATH, CONCATENATE_EMBEDDED_FILE_PATH)
+        self.insert_data('concatenate.pdf', self.config.vector_store.CONCATENATE_VECTOR_FILE_PATH)
         logger.info("Table created successfully")
 
 
 
-    def insert_data(self, file_name:str, file_path:str, vector_path:str):
+    def insert_data(self, file_name:str, vector_path:str):
         # SQL command to insert the data in the table
-        sql_command = """INSERT INTO document (name, file_path, vector_path) VALUES (?, ?, ?);"""
+        sql_command = """INSERT INTO document (name, vector_path) VALUES (?, ?, ?);"""
         # execute the statement
-        self.crsr.execute(sql_command, (file_name, file_path, vector_path))
+        self.crsr.execute(sql_command, (file_name, vector_path))
         # commit the statement
         self.connection.commit()
 
-    def update_data(self, id:int, file_path:str, vector_path:str):
+    def update_data(self, id:int, vector_path:str):
         # SQL command to update the data in the table
-        sql_command = f"""UPDATE document SET file_path = "{file_path}", vector_path = "{vector_path}" WHERE id = {id};"""
+        sql_command = f"""UPDATE document SET vector_path = "{vector_path}" WHERE id = {id};"""
         # execute the statement
         self.crsr.execute(sql_command)
         # commit the statement
@@ -63,7 +59,7 @@ class Database:
         # store all the fetched data in the ans variable
         ans = self.crsr.fetchall()
         # loop to print all the data
-        data = [{"id":i[0], "name": i[1], "file_path": i[2], "vector_path": i[3]} for i in ans]
+        data = [{"id":i[0], "name": i[1], "vector_path": i[2]} for i in ans]
         return data
 
 
@@ -75,7 +71,7 @@ class Database:
 if __name__ == "__main__":
     db = Database()
     db.create_table()
-    db.insert_data("test", "test", "test")
+    db.insert_data("test", "test")
     print(db.get_data())
     db.close_connection()
 
